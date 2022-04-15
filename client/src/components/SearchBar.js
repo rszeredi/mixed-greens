03b80e85bd5a9a88e3330fef4c858c7a15dcd39a
Ria Szeredi <ria.szeredi@gmail.com>
2022-04-15 08:57:00 -0700

    attempt at debouncing

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -5,6 +5,8 @@ import { useStateValue } from '../contexts/StateProvider';
 
 import './SearchBar.css';
 
+let searchTimeout;
+
 function SearchBar() {
 	const [ { spotify }, reducer ] = useStateValue();
 	const [ search, setSearch ] = useState(''); // todo: should these be moved to state provider?
@@ -12,28 +14,30 @@ function SearchBar() {
 
 	useEffect(
 		() => {
+			clearTimeout(searchTimeout);
 			if (!search) return setSearchResults([]);
 
-			spotify.searchTracks(search).then((res) => {
-				const _searchResults = res.body.tracks.items.map((track) => {
-					const smallesAlbumImage = track.album.images.reduce((smallest, image) => {
-						if (image.height < smallest.height) return image;
-						else return smallest;
-					}, track.album.images[0]);
+			searchTimeout = setTimeout(() => {
+				console.log('searching: ', search);
+				spotify.searchTracks(search).then((res) => {
+					console.log(res);
 
-					return {
-						artist: track.artists[0].name,
-						title: track.name,
-						uri: track.uri,
-						albumUrl: smallesAlbumImage.url
-					};
+					const _searchResults = res.body.tracks.items.map((track) => {
+						const smallesAlbumImage = track.album.images.reduce((smallest, image) => {
+							if (image.height < smallest.height) return image;
+							else return smallest;
+						}, track.album.images[0]);
+
+						return {
+							artist: track.artists[0].name,
+							title: track.name,
+							uri: track.uri,
+							albumUrl: smallesAlbumImage.url
+						};
+					});
+					setSearchResults(_searchResults);
 				});
-				setSearchResults(_searchResults);
-			});
-
-			return () => {
-				'';
-			};
+			}, 500);
 		},
 		[ search ]
 	);
@@ -42,6 +46,7 @@ function SearchBar() {
 		setSearch(e.target.value);
 	};
 
+	// console.log('searchResults', searchResults);
 	return (
 		<Container className="d-flex flex-column py-2">
 			<Form.Control
